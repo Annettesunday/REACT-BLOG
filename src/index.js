7c965e952d09fb8cd410fa6273439946fceb91f6
annettesunday <annette.odhiambo@andela.com>
2019-07-11 22:48:03 +0300

    ch(index): setting up redux thunk

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import reactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 reactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector("#root")
