51a6d6de2538067e61e0d8cb60326858284b8e0e
annettesunday <annette.odhiambo@andela.com>
2019-07-12 14:59:21 +0300

    ch(index): implement one time memoization using lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
@@ -6,10 +7,13 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// private function that should not be called by other engineers unless they know what they are doing
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   });
-};
+});
