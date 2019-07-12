fed92e0a80793b7d1812bc95bfa314aa1f6bd290
annettesunday <annette.odhiambo@andela.com>
2019-07-12 13:37:58 +0300

    ch(UserHeader): displaying user header

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
-
-
   renderList() {
-    return this.props.posts.map((post) => {
+    return this.props.posts.map(post => {
       return (
         <div className="item" key={post.id}>
           <i className="large middle aligned icon user" />
@@ -18,16 +17,20 @@ class PostList extends React.Component {
             <div className="description">
               <h2>{post.title}</h2>
               <p>{post.body}</p>
+              <UserHeader userId={post.userId} />
             </div>
           </div>
         </div>
       );
-    })
+    });
   }
   render() {
-    console.log(this.props.posts);
-
-    return <div className="ui relaxed divided list">{this.renderList()}</div>;
+    return (
+      <>
+        <div className="ui relaxed divided list">{this.renderList()}</div>
+        
+      </>
+    );
   }
 }
 
