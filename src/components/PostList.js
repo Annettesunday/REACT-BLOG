b230d73ec688609640a8c30731a9d8563be78710
annettesunday <annette.odhiambo@andela.com>
2019-07-12 12:18:49 +0300

    ch(src): dispatching correct values

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,11 +7,18 @@ class PostList extends React.Component {
     this.props.fetchPosts();
   }
   render() {
+    console.log(this.props.posts);
     return <div>PostList</div>;
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from "redux";
+import postsReducer from "./postsReducer";
 
 
 export default combineReducers({
-    replaceMe: () => 'Hi there'
+    posts: postsReducer
 })
